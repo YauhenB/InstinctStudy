4c449561f06e978197528768694f8143c287d835
yauhen <yauhen.biahanski@instinctools.ru>
2016-12-14 15:24:30 +0300

    Fixed concurrency

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,7 +1,19 @@
-package PACKAGE_NAME;
+import org.apache.log4j.Logger;
 
 /**
  * Created by yauhen on 14.12.16.
  */
-public class Consumer {
+public class Consumer extends Thread {
+    private static final Logger log = Logger.getLogger(Consumer.class);
+    private Product product;
+
+    public Consumer(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public void run() {
+        if (this.product.getValue() > 5)
+            this.product.decValue();
+    }
 }
--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -1,7 +1,19 @@
-package PACKAGE_NAME;
+import org.apache.log4j.Logger;
 
 /**
  * Created by yauhen on 14.12.16.
  */
-public class Producer {
+public class Producer extends Thread {
+    private static final Logger log = Logger.getLogger(Producer.class);
+    private Product product;
+
+    public Producer(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public void run() {
+        if (product.getValue() < 10)
+            product.incValue();
+    }
 }
